a4175081efd806eb01e6df3fb7ed5e5723de0759
isaGarz <isabelazaira123@gmail.com>
2023-10-28 16:38:53 -0300

    nav mobile animacao

--- FlorestalCode/code.js
+++ FlorestalCode/code.js
@@ -1,6 +1,25 @@
+const links = document.querySelector(".links-paginas");
+const cor = links.style.getPropertyValue("display");
+if(cor==style.display("flex")){
+    console.log("Oi");
+}else{
+    console.log("tchau");
+    console.log(cor);
+}
+
+
+
 function mostrarLinks(classe) {
-    const links = document.querySelector(".links-paginas");
+
+
+
     const estado= links.style.display;
     links.style.display = estado === "none" ? "block" : "none";
+
+
+
+
+
+
     
 }
